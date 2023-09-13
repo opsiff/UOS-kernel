42e77abfabcf1ecc658ad240b201d8a444bfc8bc
Zhang Rui <rui.zhang@intel.com>
2023-09-13 23:32:39 +0800

    tools/power/turbostat: Introduce probe_pm_features()
    commit 7ee39d8d593e3d28eced0fa1a8c8c6bdcbd4156e upstream.
    
    Feature probe has nothing to do with CPUID, thus it should not be in
    process_cpuids().
    
    Introduce probe_pm_features() and move all feature probing functions
    into it.
    
    deepin-Intel-SIG: commit 7ee39d8d593e ("tools/power/turbostat: Introduce probe_pm_features()").
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Reviewed-by: Len Brown <len.brown@intel.com>
    [ Jianfeng Gao: amend commit log ]
    Signed-off-by: Jianfeng Gao <jianfeng.gao@intel.com>
    Signed-off-by: WangYuli <wangyuli@uniontech.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -5617,7 +5617,10 @@ void process_cpuid()
 
 	BIC_PRESENT(BIC_IRQ);
 	BIC_PRESENT(BIC_TSC_MHz);
+}
 
+void probe_pm_features(void)
+{
 	probe_pstates();
 
 	probe_cstates();
@@ -5637,8 +5640,6 @@ void process_cpuid()
 
 	if (!quiet)
 		decode_misc_feature_control();
-
-	return;
 }
 
 /*
@@ -5918,6 +5919,7 @@ void turbostat_init()
 	check_dev_msr();
 	check_permissions();
 	process_cpuid();
+	probe_pm_features();
 	linux_perf_init();
 
 	for_all_cpus(get_cpu_type, ODD_COUNTERS);
