33b7e506b9d8222476d9c736df062e42351bc438
Lucas Segarra Fernandez <lucas.segarra.fernandez@intel.com>
2023-10-04 12:09:20 +0200

    crypto: qat - add pm_status debugfs file
    commit e079231676e0 ("crypto: qat - add pm_status debugfs file") upstream
    
    QAT devices implement a mechanism that allows them to go autonomously
    to a low power state depending on the load.
    
    Expose power management info by providing the "pm_status" file under
    debugfs. This includes PM state, PM event log, PM event counters, PM HW
    CSRs, per-resource type constrain counters and per-domain power gating
    status specific to the QAT device.
    
    This information is retrieved from (1) the FW by means of
    ICP_QAT_FW_PM_INFO command, (2) CSRs and (3) counters collected by the
    device driver.
    
    In addition, add logic to keep track and report power management event
    interrupts and acks/nacks sent to FW to allow/prevent state transitions.
    
    Intel-SIG: commit e079231676e0 ("crypto: qat - add pm_status debugfs file)"
    Backport to support Intel QAT in-tree driver
    
    Signed-off-by: Lucas Segarra Fernandez <lucas.segarra.fernandez@intel.com>
    Reviewed-by: Giovanni Cabiddu <giovanni.cabiddu@intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    [ Aichun Shi: amend commit log ]
    Signed-off-by: Aichun Shi <aichun.shi@intel.com>

--- drivers/crypto/intel/qat/qat_common/adf_dbgfs.c
+++ drivers/crypto/intel/qat/qat_common/adf_dbgfs.c
@@ -8,6 +8,7 @@
 #include "adf_dbgfs.h"
 #include "adf_fw_counters.h"
 #include "adf_heartbeat_dbgfs.h"
+#include "adf_pm_dbgfs.h"
 
 /**
  * adf_dbgfs_init() - add persistent debugfs entries
@@ -54,6 +55,7 @@ void adf_dbgfs_add(struct adf_accel_dev *accel_dev)
 	if (!accel_dev->is_vf) {
 		adf_fw_counters_dbgfs_add(accel_dev);
 		adf_heartbeat_dbgfs_add(accel_dev);
+		adf_pm_dbgfs_add(accel_dev);
 	}
 }
 
@@ -64,6 +66,7 @@ void adf_dbgfs_add(struct adf_accel_dev *accel_dev)
 void adf_dbgfs_rm(struct adf_accel_dev *accel_dev)
 {
 	if (!accel_dev->is_vf) {
+		adf_pm_dbgfs_rm(accel_dev);
 		adf_heartbeat_dbgfs_rm(accel_dev);
 		adf_fw_counters_dbgfs_rm(accel_dev);
 	}
