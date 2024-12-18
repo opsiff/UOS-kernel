01029cf78234e1278c1193682ee2464a07a71233
Luo Gengkun <luogengkun2@huawei.com>
2024-12-18 09:45:33 +0000

    deepin: KABI: KABI reservation for uprobe.h
    reserve space for uprobes.h
    
    Link: https://gitee.com/src-openeuler/kernel/issues/IBC1PM
    Signed-off-by: Luo Gengkun <luogengkun2@huawei.com>
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- include/linux/uprobes.h
+++ include/linux/uprobes.h
@@ -47,6 +47,7 @@ struct uprobe_consumer {
 
 #ifdef CONFIG_UPROBES
 #include <asm/uprobes.h>
+#include <linux/deepin_kabi.h>
 
 enum uprobe_task_state {
 	UTASK_RUNNING,
@@ -80,6 +81,14 @@ struct uprobe_task {
 
 	struct return_instance		*return_instances;
 	unsigned int			depth;
+	DEEPIN_KABI_RESERVE(1)
+	DEEPIN_KABI_RESERVE(2)
+	DEEPIN_KABI_RESERVE(3)
+	DEEPIN_KABI_RESERVE(4)
+	DEEPIN_KABI_RESERVE(5)
+	DEEPIN_KABI_RESERVE(6)
+	DEEPIN_KABI_RESERVE(7)
+	DEEPIN_KABI_RESERVE(8)
 };
 
 struct return_instance {
