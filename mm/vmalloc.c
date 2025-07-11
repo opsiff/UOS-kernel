11fe74d5f39b9047d8347de47569ee8b6d9640ee
Wentao Guan <guanwentao@uniontech.com>
2025-07-11 10:38:45 +0800

    BACKPORT: mm: vmalloc: add a shrinker to drain vmap pools
    mainline inclusion
    from mainline-v6.9-rc1
    commit 7679ba6b36dbb300b757b672d6a32a606499e14b
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9CHG1
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=7679ba6b36dbb300b757b672d6a32a606499e14b
    
    -------------------------------------------------
    
    The added shrinker is used to return back current cached VAs into a global
    vmap space, when a system enters into a low memory mode.
    
    Link: https://lkml.kernel.org/r/20240102184633.748113-12-urezki@gmail.com
    Signed-off-by: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Cc: Kazuhito Hagio <k-hagio-ab@nec.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Joel Fernandes (Google) <joel@joelfernandes.org>
    Cc: Liam R. Howlett <Liam.Howlett@oracle.com>
    Cc: Lorenzo Stoakes <lstoakes@gmail.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Oleksiy Avramchenko <oleksiy.avramchenko@sony.com>
    Cc: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 7679ba6b36dbb300b757b672d6a32a606499e14b)
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    
    [Since the struct shrinker was not introduced in OLK6.6, the vmalloc
    lock contention series of patches conflict. Fix this problem by
    introducing a struct shrinker.]
    
    Conflicts:
    	mm/vmalloc.c
    
    Signed-off-by: ZhangPeng <zhangpeng362@huawei.com>
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -4928,6 +4928,34 @@ static void vmap_init_nodes(void)
 	}
 }
 
+static unsigned long
+vmap_node_shrink_count(struct shrinker *shrink, struct shrink_control *sc)
+{
+	unsigned long count;
+	struct vmap_node *vn;
+	int i, j;
+
+	for (count = 0, i = 0; i < nr_vmap_nodes; i++) {
+		vn = &vmap_nodes[i];
+
+		for (j = 0; j < MAX_VA_SIZE_PAGES; j++)
+			count += READ_ONCE(vn->pool[j].len);
+	}
+
+	return count ? count : SHRINK_EMPTY;
+}
+
+static unsigned long
+vmap_node_shrink_scan(struct shrinker *shrink, struct shrink_control *sc)
+{
+	int i;
+
+	for (i = 0; i < nr_vmap_nodes; i++)
+		decay_va_pool_node(&vmap_nodes[i], true);
+
+	return SHRINK_STOP;
+}
+
 void __init vmalloc_init(void)
 {
 	struct vmap_area *va;
@@ -4977,4 +5005,7 @@ void __init vmalloc_init(void)
 	 */
 	vmap_init_free_space();
 	vmap_initialized = true;
+
+	if (register_shrinker(&vmap_node_shrinker, "vmap-node"))
+		pr_err("Failed to allocate vmap-node shrinker!\n");
 }
