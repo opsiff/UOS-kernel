09cffcee3770ff7de4b867544e6fe3185da3e73b
guanwentao <guanwentao@uniontech.com>
2026-09-14 22:42:33 +0800

    rtc: guard the poweroff alarm variable when the vendor rtc owns it
    rtc_pmic_spmi.c defines poweroff_rtc_alarm itself when
    CONFIG_RTC_DISABLE_POWEROFF_ALARM or CONFIG_PRODUCT_ARMPC is enabled, while
    alarmtimer.c defined it unconditionally, so the link failed with:
    
      ld: rtc_pmic_spmi.o: multiple definition of `poweroff_rtc_alarm

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -71,7 +71,9 @@ static DEFINE_SPINLOCK(rtcdev_lock);
 
 #define ALARM_AHEAD_TIME    (60)
 #define ALARM_AFTER_TIME    (15)
+#ifndef CONFIG_RTC_DISABLE_POWEROFF_ALARM
 struct rtc_wkalrm poweroff_rtc_alarm = { 0, 0, {0} };
+#endif
 
 /*
  * set_power_on_alarm - set power on alarm value into rtc register
